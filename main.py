38cb9d309ea2ece8580ee88674733f5e0b77e958
Alar Akilbekov <alar.akilbekov@gmail.com>
2026-07-07 13:41:16 +0500

    refactor: src/ package for modules

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.normalization import normalization
+
+
+if __name__ == "__main__":
+    text = " _ Hello, world! _ "
+
+    print(f"Raw:\n{text}", end="\n\n")
+
+    # Normalized text
+    text = normalization(text)
+    print(f"Normalized:\n{text}", end="\n\n")
